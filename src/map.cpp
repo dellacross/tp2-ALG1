ceb15438ce3842a4057cae747194ddfe1eec0c93
dellacross <victordella652@gmail.com>
2022-06-01 12:22:26 -0300

    fixing findmax

--- src/map.cpp
+++ src/map.cpp
@@ -1,4 +1,5 @@
 #include "map.hpp"
+#include <queue>
 
 Map::Map(fstream &file)
 {
@@ -38,9 +39,10 @@ void Map::findMax(int o, int d)
 {
     o--;
     d--;
-    int aux = o, heavy = 0, radar = 1, res = 0;
+    int aux = o, c = -1, heavy = 0, radar = 1, pa = 0;
     bool visited[numberOfCitys];
     int path[numberOfCitys - 1];
+    queue<int> pp;
 
     for (int i = 0; i < numberOfCitys; i++)
         visited[i] = false;
@@ -55,40 +57,62 @@ void Map::findMax(int o, int d)
     {
         for (int j = 0; j < numberOfCitys; j++)
         {
-            if (mapMatrix[aux][j] > heavy && !visited[j])
+            if (((pa == 0 && mapMatrix[aux][j] > heavy) || (pa != 0 && mapMatrix[aux][j] <= pa)) && !visited[j])
             {
                 heavy = mapMatrix[aux][j];
-                aux = j;
+                c = j;
             }
+
+            if(j == d)
+                pp.push(mapMatrix[aux][j]);
         }
 
+        aux = c;
         visited[aux] = true;
         if (heavy == 0)
         {
             path[radar] = -1;
             radar--;
+            aux = path[radar];
         }
         else
         {
             path[radar] = aux;
             radar++;
+            if(pa == 0 || heavy <= pa)
+                pa = heavy;
+            cout << "pa: " << pa << "\n";
         }
-        res = heavy;
         heavy = 0;
+        c = -1;
     }
-    cout << res << "\n";
+
+    pp.push(pa);
+    int maior = 0;
+
+    cout << "Vector:" << "\n";
+    while(!pp.empty())
+    {
+        cout << pp.front() << " ";
+        if(pp.front() > maior)
+            maior = pp.front();
+        pp.pop();
+    }
+    
+    cout << "\n" << maior << "\n";
 }
 
 void Map::leitor(fstream &file)
 {
     string line = "";
-    int origem, destino;
+    int source, destiny;
     for (int i = 0; i < numberOfQueries; i++)
     {
         getline(file, line);
         stringstream lineStream(line);
-        lineStream >> origem >> destino;
-        findMax(origem, destino);
+        lineStream >> source >> destiny;
+        cout << "source: " << source << " destiny: " << destiny << "\n";
+        findMax(source, destiny);
     }
 }
 
